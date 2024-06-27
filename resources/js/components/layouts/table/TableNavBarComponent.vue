0346e139701bbc3aa36d0179f910fc33411770d1
agusbudionoprastyo <agna4605@gmail.com>
2024-06-27 22:17:36 +0700

    ..

--- resources/js/components/layouts/table/TableNavBarComponent.vue
+++ resources/js/components/layouts/table/TableNavBarComponent.vue
@@ -10,7 +10,7 @@
 
                 <!-- <button class="webcart flex lg:hidden items-center justify-center gap-1.5 w-fit rounded-3xl capitalize text-sm font-medium h-8 px-3 transition text-white bg-heading"> -->
                     <!-- <i class="fa-solid fa-bag-shopping text-sm"></i> -->
-                <button class="webcart flex lg:hidden items-center justify-center gap-1.5 w-fit capitalize text-sm font-medium h-8 px-3 transition text-primary">
+                <button class="webcart flex lg:hidden items-center justify-center gap-1.5 w-fit capitalize text-sm font-medium h-8 px-3 transition text-primary add-btn">
                     <i class="fa-solid fa-bag-shopping text-lg text-primary"></i>
                     <span class="whitespace-nowrap">
                         {{ currencyFormat(subtotal, setting.site_digit_after_decimal_point,
